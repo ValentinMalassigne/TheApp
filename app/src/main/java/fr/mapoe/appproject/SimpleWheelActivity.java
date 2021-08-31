afe9dd0e7a8259c63f955faf8eb7806eb4b037c2
LGDRSC <valentin.malassigne3@gmail.com>
2021-08-31 18:31:13 +0200

    -légère amélioration de la roulette, ajout d'une flêche

--- app/src/main/java/fr/mapoe/appproject/SimpleWheelActivity.java
+++ app/src/main/java/fr/mapoe/appproject/SimpleWheelActivity.java
@@ -16,7 +16,7 @@ import java.util.Random;
 
 public class SimpleWheelActivity extends AppCompatActivity {
 
-    private static final String[] sectors = {"0","2","3","10","5","6","7","8","9","100","11","12"};
+    private static final String[] sectors = {"0","3","2","1","1","2","1","1","1","2","1","3"};
     private static final int[] sectorDegrees = new int[sectors.length];
     private static final Random random = new Random();
     private int degree=0;
@@ -73,7 +73,7 @@ public class SimpleWheelActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                Toast.makeText(SimpleWheelActivity.this,"Tu as "+sectors[sectors.length-(degree+1)]+" points.",Toast.LENGTH_SHORT).show();
+                Toast.makeText(SimpleWheelActivity.this,getString(R.string.you_drink)+sectors[sectors.length-(degree+1)]+getString(R.string.sips),Toast.LENGTH_SHORT).show();
                 isSpinning=false;
             }
 
