3b546454e6c29b0e94c980862dae01bd3156db05
polle <pollet.aurelien5@gmail.com>
2021-09-02 16:23:50 +0200

    ajout classement dans GameEndActivity le scrollView est inaudible

--- app/src/main/java/fr/mapoe/appproject/GameEndActivity.java
+++ app/src/main/java/fr/mapoe/appproject/GameEndActivity.java
@@ -9,7 +9,11 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -19,9 +23,6 @@ import android.widget.TextView;
 public class GameEndActivity extends AppCompatActivity {
 
     private int id = 1;
-    private TableRow rowLayout;
-    private TableLayout tableLayout;
-    private Button menuButton;
     private String[][] tabClass;
     private String[] playerTab,scoreTab;
 
@@ -32,7 +33,6 @@ public class GameEndActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_end);
         AnimationBg.startBackgroundAnimation(findViewById(R.id.score_main_layout));
-        int turnNumber = 0;
         // recuperer les données
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -40,64 +40,33 @@ public class GameEndActivity extends AppCompatActivity {
             scoreTab = extras.getStringArray("scoreTab");
 
         }
+        // init les tableaux
+        classTab();
+        printWinner();
+        
+
+        
+
+
+
+
+    }
+    @Override
+    public void onBackPressed() {
+
+        Intent mainActivity = new Intent(getApplicationContext(), fr.mapoe.appproject.MainActivity.class);
+        startActivity(mainActivity);
+        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+        finish();
+    }
+
+    private void classTab(){
         // remplir le tableau à 2 dim contenant pseudo+score
         String[][] SCORE_TAB = new String[playerTab.length][2];
         for(int i=0;i<SCORE_TAB.length;i++){
-                SCORE_TAB[i][0] = playerTab[i];
-                SCORE_TAB[i][1] = scoreTab[i];
+            SCORE_TAB[i][0] = playerTab[i];
+            SCORE_TAB[i][1] = scoreTab[i];
         }
-        
-
-        
-        this.tableLayout = (TableLayout) findViewById(R.id.idTable);
-        this.menuButton = (Button) findViewById(R.id.menu_button);
-        menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent mainActivity = new Intent(getApplicationContext(), fr.mapoe.appproject.MainActivity.class);
-                startActivity(mainActivity);
-                overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
-                finish();
-            }
-        });
-        //paramÃ¨tres
-        Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.convergence);
-        //margin
-
-
-        // ajout 1ere ligne
-        TableRow row = new TableRow(getApplicationContext());
-
-        row.setId(id);
-        tableLayout.addView(row);
-        TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams();
-        rowParams.setMargins(0,10,0,0);
-
-        row.setLayoutParams(rowParams);
-        this.rowLayout = (TableRow) findViewById(id);
-
-        TextView name = new TextView(getApplicationContext());
-        name.setText(getString(R.string.name));
-        name.setTypeface(typeface);
-        name.setTextSize(25);
-        name.setTextColor(Color.BLACK);
-        name.setTypeface(null, Typeface.BOLD);
-        name.setGravity(Gravity.CENTER);
-        rowLayout.addView(name);
-
-
-        TextView score = new TextView(getApplicationContext());
-        score.setText(getString(R.string.score));
-        score.setTypeface(typeface);
-        score.setTextSize(25);
-        score.setTextColor(Color.BLACK);
-        score.setGravity(Gravity.CENTER);
-        score.setTypeface(null,Typeface.BOLD);
-        score.setGravity(Gravity.CENTER);
-        rowLayout.addView(score);
-        id++;
-
         // refaire un tableau dans le bonne ordre
         tabClass = new String[SCORE_TAB.length][2];
         int index;
@@ -117,48 +86,72 @@ public class GameEndActivity extends AppCompatActivity {
             tabClass[SCORE_TAB.length-i-1][1]=SCORE_TAB[index][1];
             SCORE_TAB[index][1]="100000";
         }
+    }
+    //fonction pour afficher les 3 premièrs
+    private void printWinner(){
+        TextView winnerText = (TextView) findViewById(R.id.winner);
+        TextView secondText = (TextView) findViewById(R.id.second);
+        TextView thirdText = (TextView) findViewById(R.id.third);
+        ImageView medal3 = (ImageView) findViewById(R.id.medal3);
+        if(playerTab.length == 2){
+            winnerText.setText(tabClass[0][0].toString() +"\n"+tabClass[0][1]+" points");
+            secondText.setText(tabClass[1][0].toString() +"\n"+tabClass[1][1]+" points");
+            thirdText.setVisibility(View.INVISIBLE);
+            medal3.setVisibility(View.INVISIBLE);
+
+        }
+        else if(playerTab.length==3){
+            winnerText.setText(tabClass[0][0].toString() +"\n"+tabClass[0][1]+" point(s)");
+            secondText.setText(tabClass[1][0].toString() +"\n"+tabClass[1][1]+" point(s)");
+            thirdText.setText(tabClass[2][0].toString() +"\n"+tabClass[2][1]+" point(s)");
+        }
+        else{
+            winnerText.setText(tabClass[0][0].toString() +"\n"+tabClass[0][1]+" point(s)");
+            secondText.setText(tabClass[1][0].toString() +"\n"+tabClass[1][1]+" point(s)");
+            thirdText.setText(tabClass[2][0].toString() +"\n"+tabClass[2][1]+" point(s)");
+            setUpTab();
+        }
+
+    }
+    // foncton qui crée le tableau
+    private void setUpTab(){
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.idTable);
+        Typeface typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.convergence);
+        TableLayout.LayoutParams rowParams = new TableLayout.LayoutParams();
+        rowParams.setMargins(25,10,0,0);
 
         // ajout des elements 1 par 1
-        this.tableLayout = (TableLayout) findViewById(R.id.idTable);
-        typeface = ResourcesCompat.getFont(getApplicationContext(), R.font.convergence);
-        for(int i=0;i<SCORE_TAB.length;i++) {
 
-            row = new TableRow(getApplicationContext());
+        for(int i=3;i<tabClass.length;i++) {
+
+            //ViewGroup.MarginLayoutParams textParams;
+            //textParams.leftMargin = 50;
+            TableRow row = new TableRow(getApplicationContext());
             row.setLayoutParams(rowParams);
             row.setId(id);
             tableLayout.addView(row);
+            TextView name = new TextView(getApplicationContext());
 
-            row.setLayoutParams(rowParams);
-            this.rowLayout = (TableRow) findViewById(id);
-
-            name = new TextView(getApplicationContext());
             name.setText(tabClass[i][0]);
             name.setTypeface(typeface);
             name.setTextSize(25);
-            name.setTextColor(Color.BLACK);
-            name.setGravity(Gravity.CENTER);
-            rowLayout.addView(name);
+            name.setTextColor(Color.WHITE);
+            name.setPadding(20,20,20,20);
+            //name.setLayoutParams(textParams);
+            row.addView(name);
 
 
-
-            score = new TextView(getApplicationContext());
+            TextView score = new TextView(getApplicationContext());
             score.setText(tabClass[i][1]);
             score.setTypeface(typeface);
             score.setTextSize(25);
-            score.setTextColor(Color.BLACK);
+            score.setTextColor(Color.WHITE);
             score.setGravity(Gravity.CENTER);
-            rowLayout.addView(score);
+            score.setPadding(20,20,20,20);
+            row.addView(score);
 
             id++;
         }
 
-    }
-    @Override
-    public void onBackPressed() {
-
-        Intent mainActivity = new Intent(getApplicationContext(), fr.mapoe.appproject.MainActivity.class);
-        startActivity(mainActivity);
-        overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
-        finish();
     }
 }
